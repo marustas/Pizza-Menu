2ecc0f28139372090c03b987cf3b2c5f1f1c267f
Stanislau Marudau <marudov.stas@gmail.com>
2023-07-18 20:55:57 +0300

    Pizza component created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { pizzaData } from "./data.js";
+const { name, ingredients, price, photoName } = pizzaData[0];
 function App() {
   return (
     <div>
       <h1>Hello, React</h1>
-      <Pizza />
+      <Pizza
+        name={name}
+        ingredients={ingredients}
+        price={price}
+        photoName={photoName}
+      />
     </div>
   );
 }
 
-function Pizza() {
-  return <h2>Pizza</h2>;
+function Pizza({ name, ingredients, price, photoName }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{ingredients}</p>
+      <p>{price}</p>
+      <img alt="Pizza Photo" src={photoName} />
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
