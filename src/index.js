574e5ec886d67a62b715981795246f345f686006
Stanislau Marudau <marudov.stas@gmail.com>
2023-07-21 21:56:40 +0300

    Conditional rendering implemented

--- src/index.js
+++ src/index.js
@@ -41,9 +41,16 @@ const Menu = () => {
 };
 
 const Footer = () => {
+  const hour = new Date().getHours;
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open
+      {isOpen && (
+        <p>We're open until {closeHour}:00. Come visit us or order online</p>
+      )}
+      .
     </footer>
   );
 };
