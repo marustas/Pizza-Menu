5b54aeec01a009fc806258603c0ca466486919d5
Stanislau Marudau <marudov.stas@gmail.com>
2023-07-21 22:05:03 +0300

    Button for ordering added

--- src/index.js
+++ src/index.js
@@ -41,16 +41,20 @@ const Menu = () => {
 };
 
 const Footer = () => {
-  const hour = new Date().getHours;
+  const hour = new Date().getHours();
   const openHour = 12;
-  const closeHour = 22;
+  const closeHour = 24;
   const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
   return (
     <footer className="footer">
       {isOpen && (
-        <p>We're open until {closeHour}:00. Come visit us or order online</p>
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
       )}
-      .
     </footer>
   );
 };
